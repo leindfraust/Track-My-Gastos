bcc58321309263d9e9ac9f69cd701501ffd3d93c
Leindfraust <rozer223@gmail.com>
2022-02-28 15:04:35 +0800

    Create BackButton.vue

---
+++ src/components/BackButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-buttons slot="start">
+                <ion-back-button id="button" @click="homeNav"></ion-back-button>
+            </ion-buttons>
+        </ion-toolbar>
+    </ion-header>
+</template>
+<script lang="ts">
+import { IonHeader, IonToolbar, IonButtons, IonBackButton } from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BackButton',
+    components: {
+        IonHeader,
+        IonToolbar,
+        IonButtons,
+        IonBackButton
+    },
+    methods: {
+        async homeNav(){
+            await this.$router.push('/')
+            location.reload()
+        }
+    }
+})
+</script>
+<style scoped>
+#button {
+    border-color: transparent !important;
+    padding: 0 !important
+}
+</style>
