634536f857b857eda0c9c45ea2e04c559e3d7218
Leindfraust <rozer223@gmail.com>
2022-02-28 15:04:00 +0800

    update router for all routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 import Clothing from '../views/Clothing.vue';
+import Food from '../views/Food.vue'
+import Essentials from '../views/Essentials.vue'
+import Miscellaneous from '@/views/Miscellaneous.vue';
+import About from '@/views/About.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +17,26 @@ const routes: Array<RouteRecordRaw> = [
     path: '/clothing',
     name: 'ClothingNav',
     component: Clothing
+  },
+  {
+    path: '/food',
+    name: 'FoodNav',
+    component: Food
+  },
+  {
+    path: '/essentials',
+    name: 'EssentialsNav',
+    component: Essentials
+  },
+  {
+    path: '/miscellaneous',
+    name: 'MiscellaneousNav',
+    component: Miscellaneous
+  },
+  {
+    path: '/about',
+    name: 'AboutNav',
+    component: About
   }
 ]
 
