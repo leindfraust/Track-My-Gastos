0ccf2ddc8c2cdd7be80ebcf521915d0f5b2e8566
Leindfraust <rozer223@gmail.com>
2022-03-12 18:05:28 +0800

    Alert, filter and button confirm update
    Alert now works correctly, filtered recent transaction to only current date. Confirm button is now an SVG

--- src/views/Essentials.vue
+++ src/views/Essentials.vue
@@ -3,19 +3,26 @@
         <!--Back Button-->
         <back-button />
         <ion-content>
+            <u-i-s-v-g-nav-item-curve />
             <div class="container is-fluid">
-                <h1 class="title">Essentiasls💆🏼</h1>
+                <h1 class="title">
+                    <ion-img
+                        :src="require('../../public/assets/essentials.svg')"
+                        alt="cart"
+                        id="logo"
+                    ></ion-img>&nbsp;Essentials
+                </h1>
             </div>
             <section class="container is-fluid">
                 <div
                     class="notification is-danger"
                     v-if="alertInsufficientMoney"
-                >Not enough money on your wallet to complete your transaction. Please add funds to your wallet or reset.</div>
-                <p class="subtitle is-5">Log your Essentials transactions:</p>
+                >Not enough money on your wallet to complete your transaction. Please add funds to your wallet or restart.</div>
+                <p class="subtitle is-5">Log your essential transactions:</p>
                 <div class="columns is-mobile">
                     <div class="column">
                         <div class="field">
-                            <ion-input class="input" placeholder=" Item" v-model="essentialsName"></ion-input>
+                            <ion-input class="input" placeholder=" Item" v-model="item"></ion-input>
                         </div>
                     </div>
                     <div class="column">
@@ -29,26 +36,32 @@
                         </div>
                     </div>
                 </div>
-                <div class="has-text-right">
-                    <button
-                        class="button is-success"
-                        @click="subtractMoney"
-                        :disabled="alertInsufficientMoney"
-                    >Confirm</button>
+                <div class="has-text-right animate__animated animate__fadeInUp"
+                    v-if="item !== '' && parseInt(itemMoney)">
+                    <confirm-button @click="subtractMoney"></confirm-button>
                 </div>
-                <div class="container" v-if="activities !== null">
-                    <h4 class="title is-4">Essentials transactions:</h4>
-                    <ul
-                        v-for="(activity, index) in activities.filter((x: any) => { return x.type === 'Essentials' })"
-                        :key="index"
-                    >
-                        <li>
-                            {{ activity.description }}
-                            <span
-                                v-if="activity.transaction !== 0"
-                            >PHP {{ activity.transaction }}</span>
-                            on {{ new Date(activity.date).toDateString() }}
-                        </li>
+                <div class="container" v-if="Object.keys(groupedActivities).length !== 0">
+                    <h4 class="title is-4">Recent transaction:</h4>
+
+                    <ul v-for="(activities, index) in groupedActivities" :key="index">
+                        <div
+                            class="container notification is-info"
+                            v-if="new Date(index).toDateString() === new Date().toDateString()"
+                        >
+                            <p
+                                class="label has-text-white"
+                            >Today({{ new Date(index).toDateString() }}):</p>
+                            <li
+                                class="has-text-centered"
+                                v-for="activity in activities.filter((x: any) => { return x.type === 'Essentials' })"
+                                :key="activity.date"
+                            >
+                                {{ activity.description }}
+                                <span
+                                    v-if="activity.transaction !== 0"
+                                >PHP {{ activity.transaction }}</span>
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </section>
@@ -56,18 +69,24 @@
     </ion-page>
 </template>
 <script lang="ts">
-import { IonContent, IonPage, IonInput } from '@ionic/vue'
+import { IonContent, IonPage, IonInput, IonImg } from '@ionic/vue'
 import { defineComponent } from 'vue'
 import store from '@/storage'
+import ConfirmButton from '@/components/ConfirmButton.vue'
 import BackButton from '../components/BackButton.vue'
+import UISVGNavItemCurve from '@/components/UISVGNavItemCurve.vue'
+import groupBy from 'lodash.groupby'
 
 export default defineComponent({
-    name: 'EssentialsNav',
+    name: 'EssentialNav',
     components: {
         IonContent,
         IonPage,
         IonInput,
-        BackButton
+        BackButton,
+        UISVGNavItemCurve,
+        ConfirmButton,
+        IonImg
     },
     async mounted() {
         this.userMoney = await store.get('wallet')
@@ -75,6 +94,9 @@ export default defineComponent({
         this.userDetails = JSON.parse(this.userDetails)
         if (this.userDetails !== null) {
             this.activities = Array.prototype.reverse.call([...this.userDetails])
+            this.groupedActivities = groupBy(this.activities.sort((a: any, b: any) => {
+                return new Date(a.date).getTime() - new Date(b.date).getTime()
+            }).filter((x: any) => { return new Date(x.date).toDateString() === new Date().toDateString() && x.type === 'Essentials' }), "date")
         } else {
             this.userDetails = []
         }
@@ -85,8 +107,9 @@ export default defineComponent({
             userDetails: [] as any,
             activities: [] as any,
             itemMoney: '',
-            essentialsName: '',
-            alertInsufficientMoney: false
+            item: '',
+            alertInsufficientMoney: false,
+            groupedActivities: [] as any
         }
     },
     methods: {
@@ -99,20 +122,30 @@ export default defineComponent({
                 this.userMoney = await store.get('wallet')
                 await this.userDetails.push({
                     type: 'Essentials',
-                    description: `Bought ${this.essentialsName} for`,
+                    description: `${this.item} for`,
                     transaction: this.itemMoney,
-                    date: new Date()
+                    date: new Date().toLocaleDateString()
                 });
                 await store.set('activities', JSON.stringify(this.userDetails))
                 this.userDetails = await store.get('activities')
                 this.userDetails = await JSON.parse(this.userDetails)
                 if (await this.userDetails !== null) {
                     this.activities = Array.prototype.reverse.call([...this.userDetails])
+                    this.groupedActivities = groupBy(await this.activities.sort((a: any, b: any) => {
+                        return new Date(a.date).getTime() - new Date(b.date).getTime()
+                    }).filter((x: any) => { return new Date(x.date).toDateString() === new Date().toDateString() && x.type === 'Essentials' }), "date")
                 }
-                this.essentialsName = ''
+                this.item = ''
                 this.itemMoney = ''
+                this.alertInsufficientMoney = false
             }
         }
     }
 })
 </script>
+<style scoped>
+#logo {
+    width: 15%;
+    display: inline-block;
+}
+</style>
--- src/views/Food.vue
+++ src/views/Food.vue
@@ -3,19 +3,22 @@
         <!--Back Button-->
         <back-button />
         <ion-content>
+            <u-i-s-v-g-nav-item-curve />
             <div class="container is-fluid">
-                <h1 class="title">Food🍔</h1>
+                <h1 class="title">
+                    <ion-img :src="require('../../public/assets/food.svg')" alt="cart" id="logo"></ion-img>&nbsp;Foods
+                </h1>
             </div>
             <section class="container is-fluid">
                 <div
                     class="notification is-danger"
                     v-if="alertInsufficientMoney"
-                >Not enough money on your wallet to complete your transaction. Please add funds to your wallet or reset.</div>
-                <p class="subtitle is-5">Log your Food transactions:</p>
+                >Not enough money on your wallet to complete your transaction. Please add funds to your wallet or restart.</div>
+                <p class="subtitle is-5">Log your food transactions:</p>
                 <div class="columns is-mobile">
                     <div class="column">
                         <div class="field">
-                            <ion-input class="input" placeholder=" Item" v-model="foodName"></ion-input>
+                            <ion-input class="input" placeholder=" Item" v-model="item"></ion-input>
                         </div>
                     </div>
                     <div class="column">
@@ -29,26 +32,32 @@
                         </div>
                     </div>
                 </div>
-                <div class="has-text-right">
-                    <button
-                        class="button is-success"
-                        @click="subtractMoney"
-                        :disabled="alertInsufficientMoney"
-                    >Confirm</button>
+                <div class="has-text-right animate__animated animate__fadeInUp"
+                    v-if="item !== '' && parseInt(itemMoney)">
+                    <confirm-button @click="subtractMoney"></confirm-button>
                 </div>
-                <div class="container" v-if="activities !== null">
-                    <h4 class="title is-4">Food transactions:</h4>
-                    <ul
-                        v-for="(activity, index) in activities.filter((x: any) => { return x.type === 'Food' })"
-                        :key="index"
-                    >
-                        <li>
-                            {{ activity.description }}
-                            <span
-                                v-if="activity.transaction !== 0"
-                            >PHP {{ activity.transaction }}</span>
-                            on {{ new Date(activity.date).toDateString() }}
-                        </li>
+                <div class="container" v-if="Object.keys(groupedActivities).length !== 0">
+                    <h4 class="title is-4">Recent transaction:</h4>
+
+                    <ul v-for="(activities, index) in groupedActivities" :key="index">
+                        <div
+                            class="container notification is-info"
+                            v-if="new Date(index).toDateString() === new Date().toDateString()"
+                        >
+                            <p
+                                class="label has-text-white"
+                            >Today({{ new Date(index).toDateString() }}):</p>
+                            <li
+                                class="has-text-centered"
+                                v-for="activity in activities"
+                                :key="activity.date"
+                            >
+                                {{ activity.description }}
+                                <span
+                                    v-if="activity.transaction !== 0"
+                                >PHP {{ activity.transaction }}</span>
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </section>
@@ -56,10 +65,13 @@
     </ion-page>
 </template>
 <script lang="ts">
-import { IonContent, IonPage, IonInput } from '@ionic/vue'
+import { IonContent, IonPage, IonInput, IonImg } from '@ionic/vue'
 import { defineComponent } from 'vue'
 import store from '@/storage'
+import ConfirmButton from '@/components/ConfirmButton.vue'
 import BackButton from '../components/BackButton.vue'
+import UISVGNavItemCurve from '@/components/UISVGNavItemCurve.vue'
+import groupBy from 'lodash.groupby'
 
 export default defineComponent({
     name: 'FoodNav',
@@ -67,7 +79,10 @@ export default defineComponent({
         IonContent,
         IonPage,
         IonInput,
-        BackButton
+        BackButton,
+        UISVGNavItemCurve,
+        ConfirmButton,
+        IonImg
     },
     async mounted() {
         this.userMoney = await store.get('wallet')
@@ -75,6 +90,9 @@ export default defineComponent({
         this.userDetails = JSON.parse(this.userDetails)
         if (this.userDetails !== null) {
             this.activities = Array.prototype.reverse.call([...this.userDetails])
+            this.groupedActivities = groupBy(this.activities.sort((a: any, b: any) => {
+                return new Date(a.date).getTime() - new Date(b.date).getTime()
+            }).filter((x: any) => { return new Date(x.date).toDateString() === new Date().toDateString() && x.type === 'Food' }), "date")
         } else {
             this.userDetails = []
         }
@@ -85,8 +103,9 @@ export default defineComponent({
             userDetails: [] as any,
             activities: [] as any,
             itemMoney: '',
-            foodName: '',
-            alertInsufficientMoney: false
+            item: '',
+            alertInsufficientMoney: false,
+            groupedActivities: [] as any
         }
     },
     methods: {
@@ -99,20 +118,30 @@ export default defineComponent({
                 this.userMoney = await store.get('wallet')
                 await this.userDetails.push({
                     type: 'Food',
-                    description: `Bought ${this.foodName} for`,
+                    description: `${this.item} for`,
                     transaction: this.itemMoney,
-                    date: new Date()
+                    date: new Date().toLocaleDateString()
                 });
                 await store.set('activities', JSON.stringify(this.userDetails))
                 this.userDetails = await store.get('activities')
                 this.userDetails = await JSON.parse(this.userDetails)
                 if (await this.userDetails !== null) {
                     this.activities = Array.prototype.reverse.call([...this.userDetails])
+                    this.groupedActivities = groupBy(await this.activities.sort((a: any, b: any) => {
+                        return new Date(a.date).getTime() - new Date(b.date).getTime()
+                    }).filter((x: any) => { return new Date(x.date).toDateString() === new Date().toDateString() && x.type === 'Food' }), "date")
                 }
-                this.foodName = ''
+                this.item = ''
                 this.itemMoney = ''
+                this.alertInsufficientMoney = false
             }
         }
     }
 })
 </script>
+<style scoped>
+#logo {
+    width: 15%;
+    display: inline-block;
+}
+</style>
